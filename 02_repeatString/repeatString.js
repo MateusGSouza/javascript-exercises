d88e6bba88b92414e11836f4e1934269113de819
MateusGSouza <115435321+MateusGSouza@users.noreply.github.com>
2023-01-21 20:16:11 -0300

    Pass helloWorld and repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) {
+        return "ERROR"
+    };
+    
+    return string.repeat(num)
 };
 
+repeatString("alo", 5)
 // Do not edit below this line
 module.exports = repeatString;
