ae292d25489153449ccb4310ea781ca4c38c4e04
MateusGSouza <115435321+MateusGSouza@users.noreply.github.com>
2023-01-21 22:54:00 -0300

    Pass reverseString test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringLength = string.length-1
 
+    let stringReversed = []
+    let count = 0
+    for (let index = stringLength; index > -1; index-- ) {
+        stringReversed[count] = string.split("")[index]
+        count++
+    }
+    stringReversed = stringReversed.join().replaceAll(",", "")
+    return stringReversed
 };
 
 // Do not edit below this line
