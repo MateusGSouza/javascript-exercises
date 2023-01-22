dad02c72145fedd1453112cad5d11b93f83910c2
MateusGSouza <115435321+MateusGSouza@users.noreply.github.com>
2023-01-22 20:21:56 -0300

    Pass sumAll test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(initialNum, finalNum) {
+    let result = 0;
+    let count = 0;
+    
+    if (typeof(initialNum) != "number" || typeof(finalNum) != "number") {
+        return "ERROR"
+    };
 
+    if (initialNum < 0 || finalNum < 0) {
+        return "ERROR"
+    };
+
+    if (initialNum > finalNum) {
+        count  = finalNum;
+
+        while (count <= initialNum) {
+            result+=count
+            count++
+        };
+
+    } else {
+        count = initialNum;
+        
+        while (count <= finalNum) {
+            result+=count
+            count++
+        }
+    };
+
+    
+    return result
 };
 
 // Do not edit below this line
