5ae86ffdfaeed5a3638a5142c754e78e7ec43079
MateusGSouza <115435321+MateusGSouza@users.noreply.github.com>
2023-01-23 17:10:40 -0300

    Pass leapYears test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const centuryYear = year % 100 == 0 && year % 400 != 0;
+    const leapYear = year % 4 == 0;
 
+    if (centuryYear || !leapYear) {
+        return false
+    } else {
+        return true
+    };
 };
 
+// leapYears(34992)
 // Do not edit below this line
 module.exports = leapYears;
