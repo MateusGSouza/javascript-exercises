a123933741b7ce680afc317e28d33e3cc7516ba7
MateusGSouza <115435321+MateusGSouza@users.noreply.github.com>
2023-01-23 19:04:33 -0300

    Pass tempConversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const celsiusTemp = ( temperature - 32 ) / 1.8
+  const celsiusTempRounded = Math.round(celsiusTemp * 10) / 10
+  return celsiusTempRounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheitTemp = ( temperature * 1.8 + 32 )
+  const fahrenheitTempRounded = Math.round(fahrenheitTemp * 10) / 10
+  return fahrenheitTempRounded
 };
 
 // Do not edit below this line
